a190123f526157ded299ab8e6a24e851b7cbe7f2
William Selbie <wselbie6@gmail.com>
2018-12-31 11:58:28 -0800

    Added binary_step function for Perceptron. Removed unused imports.

--- logistic_regression.py
+++ logistic_regression.py
@@ -2,7 +2,6 @@ import math
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd
-import seaborn as sb
 
 def main():
     target_column = 'Survived'
@@ -21,9 +20,10 @@ def main():
     logistic_function = sigmoid
     weights = gradient_ascent(train_data, target_column, logistic_function, batch_size=len(train_data), epochs=10000)
     hypotheses = np.rint(logistic_function(np.matmul(test_features, weights)))
-
     results = pd.DataFrame(np.hstack((hypotheses, test_targets)))
-    print(results.corr())
+    accuracy = calculate_accuracy(results)
+
+    print(accuracy)
 
 def gradient_ascent(data, target_column, logistic_function, batch_size=1, learning_rate=0.0000001, epochs=10000):
     weights = np.zeros((data.shape[1] - 1, 1))
@@ -41,8 +41,16 @@ def gradient_ascent(data, target_column, logistic_function, batch_size=1, learni
 def calculate_log_likelihood(scores, targets):
     return np.sum(targets * scores - np.log(1 + np.exp(scores)))
 
+# Logistic Regression
 def sigmoid(scores):
     return 1 / (1 + np.exp(-scores))
+
+# Perceptron
+def binary_step(scores, h0=0):
+    return np.heaviside(scores, h0)
+
+def calculate_accuracy(results):
+    return 1.0 - sum(abs(results[0] - results[1])) / len(results)
 
 if __name__ == "__main__":
     main()
